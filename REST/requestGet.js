d6360c901f89fb9738044b398f3da7dfe6690829
Minh Bui <dr@mbui.me>
2017-03-04 15:31:30 -0600

    Add files via upload

---
+++ REST/requestGet.js
@@ -0,0 +1,10 @@
+//Author: Minh Bui
+//Date: 3/4/2016
+//Description: calling REST using javascript. This script runs with Node.js and NPM package request
+
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred 
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+  console.log('body:', body); // Print the HTML for the Google homepage. 
+});
---
+++ REST/requestPost.js
@@ -0,0 +1,19 @@
+var request = require('request');
+
+//Lets configure and request
+request({
+    url: 'http://localhost:7474/db/data/cypher', //URL to hit
+   // qs: {from: 'blog example', time: +new Date()}, //Query string data
+    method: 'POST',
+    //Lets post the following key/values as form
+    json: {
+        "query": "MATCH (x {name:'I'})-[r]->(n) RETURN type(r), n.name,n.age",
+        "params":{} 
+    }
+}, function(error, response, body){
+    if(error) {
+        console.log(error);
+    } else {
+        console.log(response.statusCode, body);
+}
+});
